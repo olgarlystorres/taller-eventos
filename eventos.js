658bcb21961fd67b98aeea77b61638057b825094
olgarlystorres <olgarlystorres@gmail.com>
2024-09-04 00:42:44 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,11 +1,18 @@
 
-const divButon = document.querySelector("container");
+document.addEventListener('DOMContentLoaded', (Event) => {
+    document.querySelector('.container').addEventListener('click', ()=> {
+        alert('Hola! Soy el Div');
+    });
+});
+
+
+/*const divButon = document.querySelector("container");
 
 function elDiv (){
     alert('Hola! soy el Div');
 }
 
 
-//divButon.onclick = elDiv();
+//divButon.onclick = elDiv();*/
 
 divButon.addEventListener("click", elDiv);
